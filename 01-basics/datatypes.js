39748b3264ffc0faaa1bdd4d44cae712f5d1dea0
vaj1805 <vibhor.a.jain2005@gmail.com>
2024-10-13 12:21:56 +0000

    datatypes

---
+++ 01-basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict";  // treat all JS code as newer version
+
+// alert(3 + 3) // wont run cuz we are using node js , not browser.
+
+let name = "vibhor"
+let age = 18
+let isLoggedIn = false
+
+// number -> 2^53 range..if more bigger number use bigint
+//string -> ""
+// boolean -> true or false
+// null -> its a type  a value. a standalone value
+// undefined -> not specified value
+// symbol : used to identify unqiue objects  in react figma etc
+// object
+
+console.log(typeof age)
+console.log(typeof null)
+// type of undefined is undefined.
+
