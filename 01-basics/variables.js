f2ad9a557f24dab0cf437f473156ad53b1d5645f
vaj1805 <vibhor.a.jain2005@gmail.com>
2024-10-13 12:03:08 +0000

    variables

---
+++ 01-basics/variables.js
@@ -0,0 +1,8 @@
+let accountId = 234554
+var accountEmail = "vigor@gmail.com"
+let accountPassword = "12533"
+accountCity = "Banaglore"
+accountState = "Karnataka"
+
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState])
+
